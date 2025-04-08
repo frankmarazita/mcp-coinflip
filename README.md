f84c60116b52afd9019c642f8db1ad9acfb394d0
Frank Marazita <fmarazita1@gmail.com>
2025-04-09 00:52:00 +1000

    added mcp sse server logic

--- README.md
+++ README.md
@@ -1,23 +1,5 @@
-# Template Bun
+# MCP Coinflip
 
-A template for creating a Bun TypeScript project in my personal style.
+An MCP SSE server for flipping a coin.
 
-It has my custom CI/CD setup and containerization for easy deployment.
-
-### Local Development
-
-_Ensure to rename the `name` property in `package.json` to your project name, this should also matche the repo name. This is important for the CI/CD pipeline._
-
-`bun dev` - Starts the development server.
-
-### GitHub Actions
-
-This project uses GitHub Actions for CI/CD. The workflow is defined in `.github/workflows/ci.yml`.
-
-The workflow is triggered on commits to the `main` branch. A container is built and pushed to the repository.
-
-### Production
-
-The production environment is set up to use the container built in the CI/CD pipeline. The container is deployed to a server using Docker Compose.
-
-A webhook is set up to trigger the deployment when a new container is pushed to the repository.
+A heads or tails is returned as a result.
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,57 @@
-import bodyParser from "body-parser";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
 import express from "express";
+
+const server = new McpServer({
+  name: "mcp-coinflip",
+  version: "1.0.0",
+});
+
+server.tool("coinflip", {}, async ({}) => {
+  const result =
+    crypto.getRandomValues(new Uint8Array(1))[0] % 2 === 0 ? "Heads" : "Tails";
+
+  return {
+    content: [
+      {
+        type: "text",
+        text: `Coin flip result: ${result}`,
+      },
+    ],
+  };
+});
 
 const app = express();
 
-app.use(bodyParser.json());
+const transports = new Map<string, SSEServerTransport>();
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.sendStatus(200);
+});
+
+app.get("/sse", async (_, res) => {
+  const transport = new SSEServerTransport("/messages", res);
+  transports.set(transport.sessionId, transport);
+
+  // console.log("SSE connection established:", transport.sessionId);
+
+  res.on("close", () => {
+    transports.delete(transport.sessionId);
+  });
+  await server.connect(transport);
+});
+
+app.post("/messages", async (req, res) => {
+  const sessionId = req.query.sessionId as string;
+  const transport = transports.get(sessionId);
+
+  // console.log("Received message:", req.query.sessionId);
+
+  if (transport) {
+    await transport.handlePostMessage(req, res);
+  } else {
+    res.status(400).send("No transport found for sessionId");
+  }
 });
 
 export { app };
